39b95f3fddb3770d0133303a330257dfd4fb9786
gilbertor <gilbertor@dpr.com>
2023-04-12 17:09:46 -0600

    Add Dictionary project

---
+++ DictionariesApp/Dictionaries/Program.cs
@@ -0,0 +1,75 @@
+﻿// Dictionaries
+
+/// Dictionary are used when
+/// 1 you need to find values by a unique key: definitions, items by id, etc.
+/// 2 you need to translate data between the key and value.
+/// 
+
+/// Dictionaries contains a set of item labeled by a key. So they are a pair of key and value.
+/// The data is unordered.
+/// They are easy to access.
+/// 
+
+/// Dictionaries are not used everywhere. 
+/// They have a purpose. But when you need them, they are very helpful.
+
+/// Declaration
+/// Dictionary <T, T>
+/// 
+
+/// Reglas 
+/// Las llaves son unicas
+
+// Example of a diccionary to find definitions
+
+Dictionary<string, string> diccionario =  new Dictionary<string, string>();
+
+diccionario["alpha"] = "First letter of the Greek alphabet.";
+diccionario["beta"] = "Second letter of the Greek alphabet.";
+diccionario["omega"] = "Last letter of the Greek alphabet.";
+
+Console.WriteLine($"Alpha is the {diccionario["alpha"]}");
+Console.WriteLine($"Omega is the {diccionario["omega"]}");
+
+
+
+
+
+// Example of finding an employee by Id
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[1] = "Jorge";
+employees[2] = "Pancho";
+employees[3] = "Perico";
+employees[10] = "Aldus";
+
+Console.WriteLine($"The Id 1 = {employees[1]}");
+Console.WriteLine($"The Id 2 = {employees[2]}");
+Console.WriteLine($"The Id 10 = {employees[10]}");
+
+
+
+// Iterate a dictionary by key and values 
+
+foreach (var employee in employees)
+{
+    Console.WriteLine("{0} {1}", employee.Key, employee.Value);
+}
+
+Console.WriteLine("End");
+
+
+// Iterate a dictionary by key
+foreach (var key in employees.Keys)
+{
+    Console.WriteLine("{0}", key);
+}
+
+
+// Iterate a dictionary by value
+foreach (var value in employees.Values)
+{
+    Console.WriteLine("{0}", value);
+}
+
---
+++ Homework8DictionaryApp/Homework8Dictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿//Tasks
+
+
+
+
+//And return the name of the user based on the ID.
+//Say bye!
+
+
+
+// Homework 8 Dictionary App
+
+
+//Welcome the user.
+Console.WriteLine("Hello, welcome to the Dicctionary App\n");
+
+
+//Fill in a dictionary set with few records.
+var employees = new Dictionary<int, string>();
+
+employees.Add(1, "Alvaro Fuentes");
+employees.Add(2, "Beto Cuevas");
+employees.Add(3,"Carlos Armendaris");
+employees.Add(4, "Dario Garcia");
+
+
+//Ask the user for their ID.
+Console.Write("Please write your ID number: ");
+
+string? idText = Console.ReadLine();
+
+bool isValid = int.TryParse(idText, out int id);
+
+if (isValid == false)
+{
+    Console.WriteLine("The Id is not a valid number {0}. Sorry!", idText);
+    return;
+}
+
+if (id > 0 && id < 5)
+{
+    Console.WriteLine($"\nValid Id #{id}, your user name is {employees[id]}");
+}
+else
+{
+    Console.WriteLine("\nThe ID is out of rage.");
+}
+
+Console.WriteLine("\nBye!");
