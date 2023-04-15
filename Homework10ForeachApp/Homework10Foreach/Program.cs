27e4c2afd4e9074553a42463dd681b56ba1f6302
gilbertor <gilbertor@dpr.com>
2023-04-14 19:11:25 -0600

    Add for and foreach homeworks

---
+++ Homework10ForeachApp/Homework10Foreach/Program.cs
@@ -0,0 +1,47 @@
+﻿// Homework 10 Foreach
+
+
+//Ask the user for their first name. Continue asking for firts names until there are no more. Then loop through each name using foreach and tell each person hello on the Conosle.
+
+
+//tasks
+//Ask the user for their first name
+//Continue asking for firts names until there are no more
+//loop through each name using foreach
+//tell each person hello on the Conosle.
+
+
+//Test Edge Cases
+
+//No name
+//more than 1 name
+
+
+
+Console.WriteLine("Hello friend, welcome to my App \n");
+
+string? firstName = string.Empty;
+List<string>? names = new();
+
+do
+{
+    Console.Write("What is your name? ");
+
+    firstName = Console.ReadLine();
+
+    names.Add(firstName);
+
+} while (firstName != string.Empty);
+
+
+foreach (string name in names)
+{
+    if (name != string.Empty)
+    {
+        Console.WriteLine($"Hello {name}!");
+    }
+}
+
+Console.WriteLine("\nGood bye!");
+
+
---
+++ Homework9ForLoopApp/Homework9ForLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿// Homework 9 FOR LOOPS
+
+
+//Task
+//Say Hello
+//Ask the user for a comma-separated list of firts names (no spaces)
+//Split the string into a string array.
+//Loop through the array and print "Hello <name>" to the console for each person.
+//say good bye
+
+
+//Test
+
+//Edga cases
+//Array is empty -> error
+//Array with 1 name -> ok
+//Array with multiple names -> ok
+
+//Revisar inicio y fin del array,
+//zero base... inicia en 0, final es final-1
+
+
+Console.WriteLine("Hello friend Welcome to my App\n ");
+
+Console.WriteLine("Please, write a list of first names separated by commas. Do not use spaces between commas. \nGood example> Rogrigo,Gonzalo,Pedro... \nBad example> Juan , Santiago , Roel...\n");
+
+string? firstNames = Console.ReadLine();
+
+if (firstNames == null || firstNames == string.Empty || firstNames[0] == ',')
+{
+    Console.WriteLine("You didn't write any name :( !");
+    return;
+}
+
+var names = firstNames.Split(',');
+
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine($"Hello {names[i]}");
+}
+
+Console.WriteLine("\nSee you later!");
+
