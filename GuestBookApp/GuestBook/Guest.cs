204e8a6645a5eb9d3d9c16272079a4e02da41f76
gilbertor <gilbertor@dpr.com>
2023-04-19 20:03:38 -0600

    Add GuestBook App

---
+++ GuestBookApp/GuestBook/Guest.cs
@@ -0,0 +1,74 @@
+﻿namespace GuestBook
+{
+    public class Guest
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to the Guest Book. Please enjoy the moment.");
+        }
+
+        public static string AskGuestName()
+        {
+            string output;
+
+            do
+            {
+                Console.Write("What is your name? ");
+
+                output = Console.ReadLine();
+
+                if (output == "")
+                {
+                    Console.WriteLine("Please, write a name.");
+                }
+
+            } while (output == "");
+
+            return output;
+        }
+
+        public static int AskGuestParty()
+        {
+            int number;
+            bool isNumber;
+
+            do
+            {
+                Console.Write("How many people are in your party? ");
+
+                string numberText = Console.ReadLine();
+
+                isNumber = int.TryParse(numberText, out number);
+
+                if (isNumber == false)
+                {
+                    Console.WriteLine("Please, provide a valid number.");
+                }
+                else if (number < 1)
+                {
+                    Console.WriteLine("The number should be grater than 0.");
+                }
+            } while (isNumber == false || number < 1);
+
+            return number;
+        }
+
+        public static Dictionary<string, int> AddEntry(Dictionary<string, int> output,string name, int total)
+        {
+            output.Add(name, total);
+
+            return output;
+        }
+
+        public static void PrintReport(Dictionary<string, int> guestBook)
+        {
+            int sum = 0;
+            foreach (var guest in guestBook)
+            {
+                Console.WriteLine(guest.Key);
+                sum += guest.Value;
+            }
+            Console.WriteLine($"Total of people at the party {sum}");
+        }
+    }
+}
---
+++ GuestBookApp/GuestBook/Program.cs
@@ -0,0 +1,68 @@
+﻿// Guest Book APP
+
+
+/// Console Guest Book App
+/// 
+
+/// Recommendations
+/// Do not assume anything. Do not get confuse by the requirements
+/// Use methods in a new Class
+/// Use dry, 
+/// Single responsibility, 
+/// right naming, 
+/// readability of code -> count a story of what is doing
+/// 
+
+/// Test 
+/// name > ""
+///      > "none"
+///      > happy edge
+/// partyNumber > notNumber
+///             > -1
+///             > happy edge
+/// lista invitados
+/// Total de invitados
+/// 
+/// OK
+
+/// Plan>
+/// 
+/// Ask the user for ther name
+/// Ask how many they are in their party (groupo)
+/// Keep track of how many people is in the party (fiesta)
+/// At the end, print out the guest list and print the total number of guests
+/// 
+
+/// Class Guest -> metodos de los invitados... 
+/// AskGuetName
+/// AskGuestParty
+/// AddEntry
+/// PrintReport
+/// 
+
+using GuestBook;
+
+string guestName;
+int partyNumber;
+Dictionary<string, int> guestBook = new();
+
+do
+{
+    Guest.Welcome(); 
+
+    guestName = Guest.AskGuestName();
+
+    if (guestName.ToLower() != "none")
+    {
+        partyNumber = Guest.AskGuestParty();
+
+        guestBook = Guest.AddEntry(guestBook, guestName, partyNumber);
+    }
+
+} while (guestName.ToLower() != "none");
+
+Guest.PrintReport(guestBook);
+
+
+
+
