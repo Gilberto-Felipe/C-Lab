30fd5824477936c3676da61dacc65dfa4ba74baf
gilbertor <gilbertor@dpr.com>
2023-04-25 18:24:12 -0600

    Add debugging app

---
+++ HomeworkDebugginApp/HomeworkDebuggin/Program.cs
@@ -0,0 +1,25 @@
+﻿// Debugging App
+
+
+int[] nums = { 1, 2, 3, 4, 5 };
+
+
+try
+{
+    Debug(nums);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+
+
+static void Debug(int[] nums)
+{
+    for (int i = 0; i <= nums.Length; i++)
+    {
+        Console.WriteLine(nums[i]);
+    }
+}
+
