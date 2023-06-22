23d268ed2169f8765db24236122a2d9e24925e06
gilbertor <gilbertor@dpr.com>
2023-06-22 10:10:13 -0600

    Add OverrideTaskApp

---
+++ OverrideTaskApp/OverrideTask/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿namespace OverrideTask
+{
+    internal partial class Program
+    {
+        public class PersonModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string FullName { get; set; }
+            public override string ToString()
+            {
+                FullName = $"{ FirstName.ToUpper() } { LastName.ToUpper() }";
+                return FullName;
+            }
+        }
+    }
+}
---
+++ OverrideTaskApp/OverrideTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OverrideTask
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel();
+
+            Console.WriteLine("Hello!");
+
+            Console.Write("What is your first name? ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            person.LastName = Console.ReadLine();
+
+            Console.WriteLine($"Your full name is: { person }");
+
+            Console.ReadLine();
+        }
+    }
+}
