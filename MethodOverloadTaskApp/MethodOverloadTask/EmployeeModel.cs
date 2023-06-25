3cd4e49f8da45d558a552bfbaa2cb0570a7a0e50
gilbertor <gilbertor@dpr.com>
2023-06-24 22:11:20 -0600

    Add MethodOverloadTaskApp

---
+++ MethodOverloadTaskApp/MethodOverloadTask/EmployeeModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadTask
+{
+    internal class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime SetStartDate()
+        {
+            return SetStartDate(true, DateTime.Now, false, DateTime.Now.AddDays(7));
+        }
+
+        public DateTime SetStartDate(DateTime date)
+        {
+            return SetStartDate(true, date, false, DateTime.Now.AddDays(7));
+        }
+
+        public DateTime SetStartDate(DateTime date, DateTime nextDate)
+        {
+            return SetStartDate(true, date, true, nextDate);
+        }
+
+        public DateTime SetStartDate(bool hasInitialDate, DateTime date, bool hasNextDate, DateTime nextDate)
+        {
+            if (hasInitialDate == false)
+            { 
+                return StartDate = DateTime.MinValue;
+            }
+
+            if (hasInitialDate && hasNextDate)
+            {
+                return StartDate = date > nextDate ? date : nextDate;
+            }
+
+            return StartDate = date;
+        }
+    }
+}
---
+++ MethodOverloadTaskApp/MethodOverloadTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new EmployeeModel
+            {
+                FirstName = "Elon",
+                LastName = "Musk",
+                Email = "genius@elon.com",
+            };
+
+            employee.StartDate = employee.SetStartDate();
+            Console.WriteLine(employee.StartDate);
+
+            string stringDate = "2023-06-24 20:36:43";
+            DateTime date = DateTime.ParseExact(stringDate, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+            employee.StartDate = employee.SetStartDate(date);
+            Console.WriteLine(employee.StartDate);
+
+            string stringNextDate = "2023-06-30 20:36:43";
+            DateTime nextDate = DateTime.ParseExact(stringNextDate, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+            employee.StartDate = employee.SetStartDate(date, nextDate);
+            Console.WriteLine(employee.StartDate);
+
+            employee.StartDate = employee.SetStartDate(hasInitialDate: true, date, true, nextDate);
+            Console.WriteLine(employee.StartDate);
+
+
+            Console.ReadLine();
+        }
+    }
+}
