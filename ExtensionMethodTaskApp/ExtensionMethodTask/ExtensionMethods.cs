c2aa2c825bc64ef918cb999a7eb38e271e2b89ca
gilbertor <gilbertor@dpr.com>
2023-06-26 10:35:41 -0600

    Add ExtensionMethodTaskApp

---
+++ ExtensionMethodTaskApp/ExtensionMethodTask/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethodTask
+{
+    internal static class ExtensionMethods
+    {
+        public static PersonModel DefaultAge(this PersonModel person, int age)
+        {
+            person.Age = age;
+            return person;
+        }
+
+        public static void PrintInfo(this PersonModel person)
+        {
+            Console.WriteLine($"First name: { person.FirstName } \nLast name: { person.LastName } \nEmail: { person.Email } \nAge:{ person.Age }");
+        }
+    }
+}
---
+++ ExtensionMethodTaskApp/ExtensionMethodTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel()
+            {
+                FirstName = "Juan",
+                LastName = "Perez",
+                Email = "jperez@gmail.com"                
+            };
+
+            person.DefaultAge(18).PrintInfo();
+
+            Console.ReadLine();          
+        }
+    }
+}
