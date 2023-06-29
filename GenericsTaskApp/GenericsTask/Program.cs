b6eba517fbe9fd3bd50f9fc6ed3c0a4e8903f387
gilbertor <gilbertor@dpr.com>
2023-06-29 12:14:46 -0600

    Add GenericsTaskApp

---
+++ GenericsTaskApp/GenericsTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var printers = new List<Printer<string, int>>();
+
+            var printer = new Printer<string, int>();
+
+            printers.Add(printer);
+            printers.Add(printer);
+            printers.Add(printer);
+
+            int counter = 1;
+            int model = 9001;
+            foreach (var item in printers)
+            {
+                printer.PrintingInfo($"Oister-{counter++}", model++);
+            }
+
+            Console.ReadLine();
+        }
+
+        internal class Printer<T, U>
+        {
+            public string Brand { get; set; }
+            public int Model { get; set; }
+
+            public void PrintingInfo(T item, U item2)
+            {
+                Console.WriteLine($"Brand: {item}; Model: {item2}");
+            }
+        }
+
+    }
+}
