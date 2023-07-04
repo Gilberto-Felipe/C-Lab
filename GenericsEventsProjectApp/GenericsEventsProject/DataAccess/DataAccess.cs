4cd2a196703fc248dc449bbd435f384702fb2927
gilbertor <gilbertor@dpr.com>
2023-07-03 19:17:29 -0600

    Add GenericEventProjectApp

---
+++ GenericsEventsProjectApp/GenericsEventsProject/DataAccess/DataAccess.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace GenericsEventsProject.DataAccess
+{
+    internal class DataAccess<T> where T : new()
+    {
+        public event EventHandler<T> BadWordFound;
+
+        public void SaveToCSV(List<T> items, string filePath)
+        {
+            List<string> rows = new List<string>();
+
+            var cols = GetHeaders(rows);
+
+            GetData(rows, cols, items);
+
+            File.WriteAllLines(filePath, rows);
+        }
+
+
+        private PropertyInfo[] GetHeaders(List<string> rows)
+        {            
+            T entry = new T();
+
+            // Get header by Reflection
+            var cols = entry.GetType().GetProperties();
+
+            string headerRow = "";
+
+            foreach (var col in cols)
+            {
+                headerRow += $",{ col.Name }";
+            }
+
+            headerRow = headerRow.Substring(1);
+
+            rows.Add(headerRow);
+
+            return cols;
+        }
+
+        private List<string> GetData(List<string> rows, PropertyInfo[] cols, List<T> items)
+        {            
+            foreach (var item in items)
+            {
+                bool areBadWords = false;
+                var row = "";
+
+                foreach (var col in cols)
+                {
+                    row += $",{col.GetValue(item)}";
+
+                    areBadWords = SearchBadWords(row);
+
+                    if (areBadWords)
+                    {
+                        BadWordFound?.Invoke(this, item);
+                        break;
+                    }                    
+                }
+
+                if (areBadWords)
+                {
+                    continue;
+                }
+
+                row = row.Substring(1);
+
+                rows.Add(row);
+            }
+
+            return rows;
+        }
+
+        private bool SearchBadWords(string row)
+        {
+            bool output = false;
+
+            if (row.ToLower().Contains("heck") || row.ToLower().Contains("shit"))
+            {
+                output = true;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ GenericsEventsProjectApp/GenericsEventsProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using GenericsEventsProject.DataAccess;
+using GenericsEventsProject.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GenericsEventsProject
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> personas = new List<PersonModel>()
+            {
+                new PersonModel { Id = "1", Name = "JuanShit", Email = "juanDoe@gmail.com" },
+                new PersonModel { Id = "2", Name = "Masha", Email = "MashaP@yandex.com" },
+                new PersonModel { Id = "3", Name = "Takita", Email = "takita@anime.com" },
+            };
+
+            List<LaptopModel> laptops = new List<LaptopModel>()
+            {
+                new LaptopModel { Manufacturer = "Toshiba", Model = "Satellite U940" },
+                new LaptopModel { Manufacturer = "DellHeck", Model = "Dell Inspiron 5050" },
+            };
+
+            var personasData = new DataAccess<PersonModel>();
+            var laptopsData = new DataAccess<LaptopModel>();
+
+            personasData.BadWordFound += PersonasData_BadWordFound;
+            laptopsData.BadWordFound += LaptopsData_BadWordFound;
+
+            string filePath = @"C:\Users\gilbertor\AppData\Local\Temp\PersonasCSV.csv";
+            personasData.SaveToCSV(personas, filePath);
+
+            filePath = @"C:\Users\gilbertor\AppData\Local\Temp\LaptopsCSV.csv";
+            laptopsData.SaveToCSV(laptops, filePath);
+
+            Console.ReadLine();
+        }
+
+        private static void LaptopsData_BadWordFound(object sender, LaptopModel e)
+        {
+            Console.WriteLine($"The record of Manufacturer {e.Manufacturer} Model {e.Model} has badwords, and was not processed.");
+        }
+
+        private static void PersonasData_BadWordFound(object sender, PersonModel e)
+        {
+            Console.WriteLine($"The record Id {e.Id} Name {e.Name} Email {e.Email} has badwords, and was not processed.");
+        }
+    }
+}
