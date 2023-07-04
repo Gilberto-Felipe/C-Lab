2afdb45b9a5d6318bbf76cf7a8bf0a15aee3410a
gilbertor <gilbertor@dpr.com>
2023-07-04 11:40:09 -0600

    Add DemoClassLibraryApp

---
+++ DemoClassLibraryApp/DemoClassLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using StandardLibrary;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to my Calculus App");
+
+Console.WriteLine(Calculus.Sumar(10,10));
+Console.WriteLine(Calculus.Restar(10, 10));
+Console.WriteLine(Calculus.Multiplicar(10, 10));
+Console.WriteLine(Calculus.Dividir(10, 10));
+
+Console.ReadLine();
---
+++ DemoClassLibraryApp/StandardLibrary/Calculus.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+
+namespace StandardLibrary
+{
+    public static class Calculus
+    {
+        public static double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Dividir(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
